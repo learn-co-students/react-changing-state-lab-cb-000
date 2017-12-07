3c7a458f16b55d493ef80407520db2d3439c78c5
tahbristol <tahbristol@gmail.com>
2017-12-06 20:55:45 -0500

    before fetch

--- components/Board.js
+++ components/Board.js
@@ -2,10 +2,32 @@ import React from 'react';
 import Field from './Field';
 
 export default class Board extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+
   render () {
     const { board, onClick } = this.props;
     return (
       <div>
+      <table>
+  <tr>
+      <td><Field /></td>
+      <td><Field /></td>
+      <td><Field /></td>
+  </tr>
+  <tr>
+      <td><Field /></td>
+      <td><Field /></td>
+      <td><Field /></td>
+  </tr>
+  <tr>
+      <td><Field /></td>
+      <td><Field /></td>
+      <td><Field /></td>
+  </tr>
+</table>
       </div>
     );
   }
